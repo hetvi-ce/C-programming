0ca9728422d5df294b9520f67e10a35c6f8000ee
hetvi-ce <hetvisangani.ce@gmail.com>
2025-07-29 09:46:33 +0530

    Add files via upload

---
+++ sixth practical/sixth practical.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float celsius,fahrenheit;
+    printf("enter temprature in celsius=");
+    scanf("%f",&celsius);
+    fahrenheit=(celsius*9/5)+32 ;
+    printf("temprature in fahrenheit is =%f",fahrenheit);
+}
