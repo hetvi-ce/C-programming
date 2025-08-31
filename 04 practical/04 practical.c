ccc87f50176d08c5416f85c65760becdfd1c9a55
hetvi-ce <hetvisangani.ce@gmail.com>
2025-08-31 20:35:02 +0530

    Add files via upload

---
+++ 04 practical/04 practical.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    char a;
+    int b;
+    float c;
+    double d;
+    printf("as per 64-bit system\n");
+    printf("size of char= %d\n", sizeof(a));
+    printf("size of int= %d\n", sizeof(b));
+    printf("size of float= %d\n", sizeof(c));
+    printf("size of double= %d\n", sizeof(d));
+}
