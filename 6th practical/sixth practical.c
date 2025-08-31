47cd84099ac6e209773dba4e8ede3680ff34e50c
hetvi-ce <hetvisangani.ce@gmail.com>
2025-08-31 16:51:23 +0530

    Delete 6th practical directory

--- 6th practical/sixth practical.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float celsius,fahrenheit;
-    printf("enter temprature in celsius=");
-    scanf("%f",&celsius);
-    fahrenheit=(celsius*9/5)+32 ;
-    printf("temprature in fahrenheit is =%f",fahrenheit);
-}
