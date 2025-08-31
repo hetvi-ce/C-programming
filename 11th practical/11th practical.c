9cf4f6091e9c7d6c3e7ea2486559ae08755a0826
hetvi-ce <hetvisangani.ce@gmail.com>
2025-08-31 16:49:50 +0530

    Delete 11th practical directory

--- 11th practical/11th practical.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float marks;
-    printf("enter your marks=");
-    scanf("%f",&marks);
-    (marks>=90)?printf("grade A"):(marks>=80)?printf("grade B")
-    :(marks>=70)?printf("grade C"):(marks>=60)? printf("grade D"):printf("grade F");
-}
--- 11th practical/11th-1 practical.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-void main()
-{
-    float marks;
-    printf("enter your marks=");
-    scanf("%f",&marks);
-    (marks>=90)?printf("grade A, CGPA=4 ,OUTSTANDING"):(marks>=80)?printf("grade B, CGPA=3, EXECELLENT")
-    :(marks>=70)?printf("grade C, CGPA=2 ,VERY GOOD"):(marks>=60)? printf("grade D, CGPA=1, GOOD"):printf("grade FAIL , NEED IMPROVEMENT");
-}
